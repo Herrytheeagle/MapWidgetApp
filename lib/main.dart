12f851831006c57abd5c2ebe4c1b3705ddd8dee3
Heritage Samuel F <herrytheeagle@gmail.com>
2018-12-15 22:26:40 +0100

    imprvd

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,88 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of our application.
-  GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563,-122.677433);
+ GoogleMapController mapController;
+ MapType _currentMapType = MapType.normal;
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+ final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Widget App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          options: GoogleMapOptions(
-            cameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+ void _onMapCreated(GoogleMapController controller) {
+   mapController = controller;
+ }
+
+ void _onMapTypeButtonPressed() {
+   if (_currentMapType == MapType.normal) {
+     mapController.updateMapOptions(
+       GoogleMapOptions(mapType: MapType.satellite),
+     );
+     _currentMapType = MapType.satellite;
+   } else {
+     mapController.updateMapOptions(
+       GoogleMapOptions(mapType: MapType.normal),
+     );
+     _currentMapType = MapType.normal;
+   }
+ }
+
+ void _onAddMarkerButtonPressed() {
+   mapController.addMarker(
+     MarkerOptions(
+       position: LatLng(
+         mapController.cameraPosition.target.latitude,
+         mapController.cameraPosition.target.longitude,
+       ),
+       infoWindowText: InfoWindowText('Random Place', '5 Star Rating'),
+       icon: BitmapDescriptor.defaultMarker,
+     ),
+   );
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text('Maps Sample App'),
+         backgroundColor: Colors.green[700],
+       ),
+       body: Stack(
+         children: <Widget>[
+           GoogleMap(
+             onMapCreated: _onMapCreated,
+             options: GoogleMapOptions(
+               trackCameraPosition: true,
+               cameraPosition: CameraPosition(
+                 target: _center,
+                 zoom: 11.0,
+               ),
+             ),
+           ),
+           Padding(
+             padding: const EdgeInsets.all(16.0),
+             child: Align(
+               alignment: Alignment.topRight,
+               child: Column(
+                 children: <Widget>[
+                   FloatingActionButton(
+                     onPressed: _onMapTypeButtonPressed,
+                     materialTapTargetSize: MaterialTapTargetSize.padded,
+                     backgroundColor: Colors.green,
+                     child: const Icon(Icons.map, size: 36.0),
+                   ),
+                   const SizedBox(height: 16.0),
+                   FloatingActionButton(
+                     onPressed: _onAddMarkerButtonPressed,
+                     materialTapTargetSize: MaterialTapTargetSize.padded,
+                     backgroundColor: Colors.green,
+                     child: const Icon(Icons.add_location, size: 36.0),
+                   ),
+                 ],
+               )
+             ),
+           ),
+         ],
+       ),
+     ),
+   );
+ }
 }
-  
